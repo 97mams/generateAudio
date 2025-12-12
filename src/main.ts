5f4ccd0de91298a3e33024ca957cf775293d320d
97mams <97mams@gmail.com>
2025-12-12 14:39:14 +0300

    feat: add header ande content

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,19 @@ import "./style.css";
 import { setupCounter } from "./counter.ts";
 
 document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
-  <div>
-  <h1>Text to Audio</h1>
-  </div>
+  <nav class="flex justify-between items-center p-4 bg-gray-800 text-white">
+    <img src="./src/asset/images/logo.svg" alt="Logo" class="w-10 h-10 rounded">
+    <img src="./src/asset/images/github.svg" alt="GitHub" class="w-10 h-10 rounded">
+  </nav>
+    <h1 class="text-4xl font-bold mt-4">Text to Audio</h1>
+    <p class="text-lg">Convert text to audio with ease.</p>
+    <div class="flex flex-col justify-center items-center">
+      <textarea class="border border-gray-300 rounded px-4 py-2 mr-4">Entrer vos text</textarea>
+      <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Convert</button>
+    </div>
+  <footer class="fixed bottom-0 w-full flex justify-center items-center p-4 bg-gray-800 text-white">
+    <p class="text-lg">Copyright © 2025 Text to Audio</p>
+  </footer>
 `;
 
 setupCounter(document.querySelector<HTMLButtonElement>("#counter")!);
