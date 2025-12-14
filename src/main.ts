0a37e8fad428813698838984e318538fe9e251f9
97mams <97mams@gmail.com>
2025-12-14 09:44:46 +0300

    fix: update navigation and button styles for consistency

--- src/main.ts
+++ src/main.ts
@@ -2,17 +2,26 @@ import { setupCounter } from "./counter.ts";
 import "./style.css";
 
 document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
-  <nav class="flex justify-between items-center p-4 bg-gray-800 text-white">
+  <nav class="flex justify-between items-center p-4 text-white">
     <img src="./src/asset/images/logo.svg" alt="Logo" class="w-10 h-10 rounded">
-    <button class="bg-muted hover:border-forground text-white font-bold py-2 px-4 rounded">
+    <button class="border border-card hover:border-card-foreground text-white font-bold py-2 px-4 rounded">
       <img src="./src/asset/images/github.svg" alt="GitHub" class="w-10 h-10 rounded">
     </button>
   </nav>
-    <h1 class="text-4xl font-bold mt-4">Text to Audio</h1>
-    <p class="text-lg">Convert text to audio with ease.</p>
-    <div class="flex flex-col justify-center items-center">
-      <textarea class="border border-gray-300 rounded px-4 py-2 mr-4">Entrer vos text</textarea>
-      <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Convert</button>
+    <div class="text-center my-8">
+      <h1 class="scroll-m-20 text-center text-4xl font-extrabold tracking-tight text-balance">Text to Audio</h1>
+      <p class="leading-7 [&:not(:first-child)]:mt-6">Convert text to audio with ease.</p>
+    </div>
+    <div class="flex justify-around">
+    <div class="flex justify-between items-start">
+      <textarea class="h-100 border border-gray-300 rounded px-4 py-2 mr-4" placeholder="Enter your text"></textarea>
+      <button class="bg-primary hover:bg-primary-foreground text-white font-bold py-2 px-4 rounded">Convert</button>
+    </div>
+    <div class="w-full max-w-md">
+      <audio controls class="w-full">
+        <source src="audio-file.mp3" type="audio/mpeg">
+        Your browser does not support the audio element.
+      </audio>  
     </div>
   <footer class="fixed bottom-0 w-full flex justify-center items-center p-4 bg-gray-800 text-white">
     <p class="text-lg">Copyright © 2025 Text to Audio</p>
