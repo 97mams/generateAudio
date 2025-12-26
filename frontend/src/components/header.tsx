316eb23e57420f54af32cfaa60d4fd007fff4fa6
97mams <97mams@gmail.com>
2025-12-26 16:18:10 +0300

    feat: add icon and build header

--- frontend/src/components/header.tsx
+++ frontend/src/components/header.tsx
@@ -1,13 +1,17 @@
+import { GithubIcon } from "./GithubIcon";
 import { ModeToggle } from "./mode-toggle";
 import { Button } from "./ui/button";
 
 export function Header() {
   return (
     <div className="w-full h-18 flex items-center justify-between px-8 border-b border-b-2-accent">
-      <img src="" alt="logo" />
-      <div>
+      <div className="flex">
+        <img src="/audio.svg" alt="logo" className="w-10 h-8" />
+        <h1 className="font-Bold size-6">Mptreo</h1>
+      </div>
+      <div className="flex gap-1">
         <Button>
-          <img src="./assets/github.svg" alt="logo_github" />
+          <GithubIcon />
         </Button>
         <ModeToggle />
       </div>
