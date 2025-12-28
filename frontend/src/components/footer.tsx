a6b8fec68a010bb67f94cdc283c1188d3809094e
97mams <97mams@gmail.com>
2025-12-28 12:25:05 +0300

    fix: position

--- frontend/src/components/footer.tsx
+++ frontend/src/components/footer.tsx
@@ -1,6 +1,6 @@
 export function Footer() {
   return (
-    <div className="w-full h-20 border-t border-t-2-accent bg-background flex items-center justify-center">
+    <div className="w-full h-20 fixed bottom-0 border-t border-t-2-accent bg-background flex items-center justify-center">
       anjaniainamamisao@gmail.com
     </div>
   );
