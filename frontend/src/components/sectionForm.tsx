8d005fa91abba3429a19f08488ab95911c378cf1
97mams <97mams@gmail.com>
2026-01-06 10:53:20 +0300

    refactor: clean up SectionForm component by removing unused code and comments

--- frontend/src/components/sectionForm.tsx
+++ frontend/src/components/sectionForm.tsx
@@ -1,13 +1,11 @@
-import { DownloadIcon } from "lucide-react";
-import { useState } from "react";
 import { Button } from "./ui/button";
 import { Textarea } from "./ui/textarea";
 
 export function SectionForm() {
-  const [isPending, setIsPending] = useState<boolean>(false);
+  // const [isPending, setIsPending] = useState<boolean>(false);
 
   async function audio(formData: FormData) {
-    const r = await fetch("http://localhost:3333/api/audio", {
+    await fetch("http://localhost:3333/api/audio", {
       method: "POST",
       headers: { "content-type": "application/json" },
       body: JSON.stringify({
@@ -15,45 +13,32 @@ export function SectionForm() {
         language: formData.get("language"),
       }),
     });
-
-    const status = await r.status;
-    if (status) {
-      setIsPending(true);
-    }
+    //
+    // const status = await r.status;
+    // if (status) {
+    //   setIsPending(true);
+    // }
   }
 
   return (
-    <div className="w-full flex items-star justify-around m-8">
-      <div className="w-md">
-        <form action={audio}>
-          <div className="w-full flex gap-2">
-            <Textarea name="text" rows={20} placeholder="your text ..." />
-            <div className="flex flex-col gap-2">
-              <select
-                className="border w-11 border-accent rounded"
-                name="language"
-              >
-                <option value="en">En</option>
-                <option value="fr">Fr</option>
-              </select>
-              <Button type="submit" variant={"secondary"}>
-                {isPending ? "Sending..." : "send"}
-              </Button>
-            </div>
+    <div className="w-md">
+      <form action={audio}>
+        <div className="w-full flex gap-2">
+          <Textarea name="text" rows={20} placeholder="your text ..." />
+          <div className="flex flex-col gap-2">
+            <select
+              className="border w-11 border-accent rounded"
+              name="language"
+            >
+              <option value="en">En</option>
+              <option value="fr">Fr</option>
+            </select>
+            <Button type="submit" variant={"secondary"}>
+              {/* {isPending ? "Sending..." : "send"} */}send
+            </Button>
           </div>
-        </form>
-      </div>
-      <div>
-        <div className="flex items-end gap-2">
-          <figure>
-            <figcaption>Listen:</figcaption>
-            <audio controls src=""></audio>
-          </figure>
-          <Button variant={"outline"}>
-            <DownloadIcon />
-          </Button>
         </div>
-      </div>
+      </form>
     </div>
   );
 }
