f18759d9b3c0f724c7873c17d57ed2d6e494e7c6
97mams <97mams@gmail.com>
2026-01-06 10:53:13 +0300

    feat: add ListItems component for audio playback and streaming

---
+++ frontend/src/components/listItem.tsx
@@ -0,0 +1,52 @@
+import { useEffect, useState } from "react";
+
+type propsType = {
+  id: number;
+  url: string;
+  download: string;
+  stream: string;
+}[];
+
+export function ListItems() {
+  const [items, setItems] = useState<propsType>();
+  useEffect(() => {
+    const fetchData = async () => {
+      const url = "http://localhost:3333/api/audio";
+      const response = await fetch(url, {
+        method: "get",
+        headers: {
+          "content-type": "application/json",
+        },
+      });
+      const data = await response.json();
+      setItems(data);
+    };
+    fetchData();
+  }, []);
+
+  const rendering = items?.map((item) => (
+    <div key={item.id}>
+      <div className="flex items-end gap-2">
+        <audio
+          controls
+          src={"http://localhost:3333/uploads/" + item.url}
+        ></audio>
+
+        {/* <figure>
+          <figcaption>Listen:</figcaption>
+          <audio src="">
+            <source src={"http://localhost:3333/api" + item.stream} />
+          </audio>
+        </figure>
+        <a href={item.download}>
+          {" "}
+          <Button variant={"outline"}>
+            <DownloadIcon />
+          </Button>
+        </a> */}
+      </div>
+    </div>
+  ));
+
+  return <div>{items?.length === 0 ? "" : rendering}</div>;
+}
