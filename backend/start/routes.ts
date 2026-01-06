5d650a5077a4382dd91f6f70d9560e2debc59dee
97mams <97mams@gmail.com>
2026-01-06 10:53:09 +0300

    feat: update audio route to use 'download' and add streaming functionality

--- backend/start/routes.ts
+++ backend/start/routes.ts
@@ -5,8 +5,9 @@ const AudioController = () => import('#controllers/audio_controller')
 router
   .group(() => {
     router.get('audio', [AudioController, 'index'])
-    router.get('audio/:id', [AudioController, 'show'])
+    router.get('audio/:id', [AudioController, 'download'])
     router.post('audio', [AudioController, 'store'])
+    router.get('audio/stream/:id', [AudioController, 'stream'])
   })
   .prefix('/api')
   .use(middleware.auth())
