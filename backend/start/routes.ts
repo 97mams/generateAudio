f8a4d99de02f1e8d1aa9c59f066ace8a6168c180
97mams <97mams@gmail.com>
2026-01-07 11:52:59 +0300

    feat: add delete endpoint

--- backend/start/routes.ts
+++ backend/start/routes.ts
@@ -8,6 +8,7 @@ router
     router.post('audio', [AudioController, 'store'])
     router.get('audio/:id/download', [AudioController, 'download'])
     router.get('audio/:id/stream', [AudioController, 'stream'])
+    router.delete('audio/:id', [AudioController, 'destroy'])
   })
   .prefix('/api')
   .use(middleware.auth())
