b00661d1d6704982e400121620ef3b0178b5c270
97mams <97mams@gmail.com>
2026-01-13 16:44:39 +0300

    feat: migrate database configuration from MySQL to PostgreSQL

--- backend/config/database.ts
+++ backend/config/database.ts
@@ -2,20 +2,21 @@ import env from '#start/env'
 import { defineConfig } from '@adonisjs/lucid'
 
 const dbConfig = defineConfig({
-  connection: 'mysql',
+  connection: 'pg',
   connections: {
-    mysql: {
-      client: 'mysql2',
+    pg: {
+      client: 'pg',
       connection: {
         host: env.get('DB_HOST'),
-        port: env.get('DB_PORT'),
+        port: Number(env.get('DB_PORT')),
         user: env.get('DB_USER'),
         password: env.get('DB_PASSWORD'),
         database: env.get('DB_DATABASE'),
       },
-      migrations: {
-        naturalSort: true,
-        paths: ['database/migrations'],
+
+      pool: {
+        min: 0,
+        max: 5,
       },
     },
   },
