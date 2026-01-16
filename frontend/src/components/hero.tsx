c9d617dbcee73f38cb38de675d79263c09079a17
97mams <97mams@gmail.com>
2026-01-16 16:10:43 +0300

    refactor: align items in Hero component for improved layout consistency

--- frontend/src/components/hero.tsx
+++ frontend/src/components/hero.tsx
@@ -1,6 +1,6 @@
 export function Hero() {
   return (
-    <div className="m-8 w-full flex-col gap-3">
+    <div className="m-8 w-full flex-col items-center gap-3">
       <h1 className="scroll-m-20 text-center text-4xl font-extrabold tracking-tight text-balance">
         Text to audio
       </h1>
