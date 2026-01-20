c5709f06d74ac0160845297ed7b7d66e5b2f930a
97mams <97mams@gmail.com>
2026-01-20 09:49:23 +0300

    refactor: enhance ListItems component with loading skeleton and isPending prop

--- frontend/src/components/listItem.tsx
+++ frontend/src/components/listItem.tsx
@@ -2,14 +2,17 @@ import type { dataType } from "@/App";
 import { DownloadIcon, Trash2 } from "lucide-react";
 import { toast } from "sonner";
 import { Button } from "./ui/button";
+import { Skeleton } from "./ui/skeleton";
 
 const API_URL = import.meta.env.API_URL;
 
 export function ListItems({
   items,
   onDelete,
+  isPending,
 }: {
   items: dataType[];
+  isPending?: boolean;
   onDelete?: (id: string) => void;
 }) {
   const handlerDelete = async (id: string) => {
@@ -27,12 +30,23 @@ export function ListItems({
     }
   };
 
+  const sekeletonRender = () => {
+    return (
+      <div className="w-md h-96 overflow-y-auto">
+        {[1].map((item) => (
+          <div key={item} className="flex items-end gap-2 mb-2">
+            <Skeleton className="w-64 h-10" />
+            <Skeleton className="w-8 h-8" />
+            <Skeleton className="w-8 h-8" />
+          </div>
+        ))}
+      </div>
+    );
+  };
+
   return (
     <div className="w-md h-96 overflow-y-auto">
-      {items.length === 0 && (
-        <p className="text-sm text-muted-foreground">No audio available</p>
-      )}
-
+      {items.length === 0 && sekeletonRender()}
       {items.map((item) => (
         <div key={item.id} className="flex items-end gap-2 mb-2">
           <audio controls preload="none">
@@ -50,6 +64,7 @@ export function ListItems({
           </Button>
         </div>
       ))}
+      {isPending && sekeletonRender()}
     </div>
   );
 }
