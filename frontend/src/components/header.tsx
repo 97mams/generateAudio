79b0ec97c78215fd9ec71fee05ef772ab53461fc
97mams <97mams@gmail.com>
2026-02-09 11:25:09 +0300

    Update audio assets and modify header component
    - Replaced the existing audio.svg with a new design.
    - Removed the title "Mptreo" from the header component for a cleaner look.

--- frontend/src/components/header.tsx
+++ frontend/src/components/header.tsx
@@ -7,7 +7,6 @@ export function Header() {
     <div className="w-full h-18 flex items-center justify-between px-8 border-b border-b-2-accent">
       <div className="flex">
         <img src="/audio.svg" alt="logo" className="w-10 h-8" />
-        <h1 className="font-Bold size-6">Mptreo</h1>
       </div>
       <div className="flex gap-1">
         <a target="_blank" href="https://github.com/97mams">
