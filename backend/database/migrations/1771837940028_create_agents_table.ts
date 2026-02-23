a4c4d71dbdf76cb8214096f558bbcf26fa809ad4
97mams <97mams@gmail.com>
2026-02-23 12:26:22 +0300

    feat(database): create agents table with necessary fields and relationships

--- backend/database/migrations/1771837940028_create_agents_table.ts
+++ backend/database/migrations/1771837940028_create_agents_table.ts
@@ -1,13 +1,12 @@
 import { BaseSchema } from '@adonisjs/lucid/schema'
 
 export default class extends BaseSchema {
-  protected tableName = 'audio'
+  protected tableName = 'agents'
 
   async up() {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id')
       table.string('name').notNullable()
-      table.integer('agent_id').unsigned().references('agents.id').onDelete('CASCADE')
       table.timestamp('created_at')
       table.timestamp('updated_at')
     })
