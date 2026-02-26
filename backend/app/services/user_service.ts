e3450ea9da14da1c95f49bd2158090542230a5d1
97mams <97mams@gmail.com>
2026-02-26 14:20:29 +0300

    feat: add function get audio by agent

--- backend/app/services/user_service.ts
+++ backend/app/services/user_service.ts
@@ -1,4 +1,5 @@
 import agent from '#models/agent'
+import Audio from '#models/audio'
 
 export class UserService {
   async createUser(user: string) {
@@ -18,5 +19,19 @@ export class UserService {
       throw new Error('User not found')
     }
     return user
+  }
+
+  async deleteUser(name: string) {
+    const user = await agent.query().where('name', name).first()
+    if (!user) {
+      throw new Error('User not found')
+    }
+    await user.delete()
+  }
+
+  async getAudioByAgent(name: string) {
+    const audios = await Audio.query().where('name', name).preload('agent')
+
+    return audios
   }
 }
