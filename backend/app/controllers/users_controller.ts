d9ab93a3d4cc1589b381ff88a9d36b90352b0474
97mams <97mams@gmail.com>
2026-03-01 20:35:17 +0300

    add users controller

---
+++ backend/app/controllers/users_controller.ts
@@ -0,0 +1,44 @@
+import Agent from '#models/agent'
+import type { HttpContext } from '@adonisjs/core/http'
+
+export default class UsersController {
+  /**
+   * Display a list of resource
+   */
+  async index({}: HttpContext) {
+    return await Agent.all()
+  }
+
+  /**
+   * Display form to create a new record
+   */
+  async create({ request }: HttpContext) {
+    const agent = await request.headers("user-agent")
+    return await Agent.create({ name: request.input('name'), userAgent: agent })
+  }
+
+  /**
+   * Handle form submission for the create action
+   */
+  async store({ request }: HttpContext) {}
+
+  /**
+   * Show individual record
+   */
+  async show({ params }: HttpContext) {}
+
+  /**
+   * Edit individual record
+   */
+  async edit({ params }: HttpContext) {}
+
+  /**
+   * Handle form submission for the edit action
+   */
+  async update({ params, request }: HttpContext) {}
+
+  /**
+   * Delete record
+   */
+  async destroy({ params }: HttpContext) {}
+}
